7f557c24fbcf005661704a43cdf0e5bc25c7d2cc
John Nguyen <nguyenhoangtu.427@gmail.com>
2024-10-24 22:54:49 -0400

    finished lab 4

--- labs/week 4/lab4-part3.s
+++ labs/week 4/lab4-part3.s
@@ -1,98 +1,86 @@
-
-#############################################################
-# THIS PROGRAM EXECUTES MULTIPLICATION VIA RECURSIVE ADDITION
-#############################################################
+##################################################################
+# THIS PROGRAM EXECUTES FACTORIAL VIA RECURSIVE MULTIPLICATION
+##################################################################
     .data
 result: .word 0
-arga:   .word 3
-argb:   .word 30
+arg:   .word 10
 
     .text
     .globl _start
 
 _start:
     # Load the values of a and b from memory
-    la s0, arga          #
-    lw s0, 0(s0)         # a = 5
-    la s1, argb          #
-    lw s1, 0(s1)         # b = 3
+    la s0, arg          #
+    lw s0, 0(s0)        # a
 
     # Call the multiply function
     mv a0, s0
-    mv a1, s1
-    jal multiply
+    jal factorial
 
     # Store the result in memory (optional, for checking)
     la t0, result
     sw a0, 0(t0)     # Store the value in result
 
     #NOW PRINT THE RESULT
-    #a0 = mult (a, b)
-    #a1 = a
-    #a2 = b
     mv a1, s0
-    mv a2, s1
     jal print_result
+
     # Exit the program
     li a0, 10         # Exit code for ecall
     ecall
 
 ########################################
 #ALGORITHM
-#  multiply(a,b):
-#    if b > a:
-#      return multiply(b, a)
-#    else if b == 0:
-#      return 0
+#  factorial(a):
+#    #base case
+#    if a == 0:
+#       return 1
+#    #recursive case
 #    else:
-#      return a + multiply(a, b-1)
+#      return a * factorial(a-1)
 #######################################
-multiply:
+
+factorial:
     # Save the current state on the stack
-    addi sp, sp, -8     # Push 2 words on stack
+    addi sp, sp, -8     # Push 1 words on stack
     sw a0, 4(sp)        # Save a
+    li a2, 1
     sw ra, 0(sp)        # Save ra
 
-    # Swap if b > a
-    bge a0, a1, check_base_case # If a >= b, skip the swap
-    mv t0, a0 # Temp register t0 = a
-    mv a0, a1 # a = b
-    mv a1, t0 # b = a
-    j multiply # Call mutliply again with a and b swapped without a return address
-
-check_base_case:
-    # Base case: b == 0
-    bne a1, zero, recursive_case
-    li   a0, 0          # Return 0
-    addi sp, sp, 8      # Pop the stack
-    jr   ra
+    # Base case: if a == 0 or a == 1, return 1
+    li t0, 1            # Load 1 into t0 for comparison
+    ble a0, t0, base_case   # If a <= 1, go to base case
 
 recursive_case:
-    # Recursive case: result += a, b--
-    addi a1, a1, -1
-     # Call the multiply function
-    jal multiply        # Recursive call mult(a, b-1)
+    # Recursive case: result = a * factorial(a--)
+    addi a0, a0, -1
+    # Call the multiply function
+    jal factorial       # Recursive call factorial(a-1)
 
     lw t0, 4(sp)        # Load the address of result into t0
-    add a0, a0, t0      # result in a0 = a + mult(a, b-1)
+    mul a0, a0, t0      # result in a0 = a * factorial(a-1)
 
     #Get ready to return from recursive call
     lw ra, 0(sp)        # Restore ra
     addi sp, sp, 8      # Pop stack
     jr ra
 
+base_case:
+    li a0, 1            # Return 1 for factorial(0) or factorial(1)
+    addi sp, sp, 8      # Pop the stack
+    jr ra               # Return
+
 #LETS HANDLE OUTPUT
 .data
     prompt:   .asciiz "The result of "
-    times:    .asciiz " * "
+    exclaimation_point:    .asciiz "!"
     equals:   .asciiz " = "
     new_line: .asciiz "\n"
 
 .text
 
 #the result will be in the a0 register
-#print results takes 3 arguments a0 = a, a1 = b, a2 = result
-#this should give you appreciation for printf in C
+#print results takes 2 arguments a0 = result, a1 = a
 print_result:
     #save a0 & a1 they are needed for syscalls
     mv t0, a0
@@ -103,19 +91,14 @@ print_result:
     la a1, prompt
     ecall
 
-    # Print arg 1 which is now in t0
+    # Print arg which is now in t1
     li a0, 1        # 1 is syscall for print_int
     mv a1, t1
     ecall
 
     #print the multiply symbox
     li a0, 4        # 4 is syscall for print_str
-    la a1, times
-    ecall
-
-    # Print arg 2 which is in a2
-    li a0, 1        # 1 is syscall for print_int
-    mv a1, a2
+    la a1, exclaimation_point
     ecall
 
     #print the multiply symbox
